42e36cc0ad8eb6bc030ff3246ad24fa3ba5973cb
artiom sokolov <asokolik87@gmail.com>
2021-03-09 17:28:33 +0300

    added header bottom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
 import Header from './components/header/Header';
 
 
@@ -19,15 +18,6 @@ function App() {
       
       </div>
     </>
-=======
-import Search from './components/Search/Search.js';
-
-function App() {
-  return (
-    <div class="container">  
-      <Search />
-    </div>
->>>>>>> f9876e8a4ae6980aa5d2f0abe9f3f9f0fbda1f8a
   );
 
 }
